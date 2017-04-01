435bdbbd038dd3fd153655ff3709882d9fd1356c
Dragos Rotaru <info@dragosrotaru.com>
2017-03-31 21:10:01 -0400

    almost done

--- mainscript.js
+++ mainscript.js
@@ -3,18 +3,12 @@ OUTPUT_MESSAGE = ' of the substitute is needed to provide the same amount of cal
 ERROR_CALORIES_MESSAGE = 'sorry, that substitute is not possible';
 OZ = 'oz';
 G = 'g';
+data = [];
 
 CALORIES_PER_GRAM = {
   food_to_be_substituted_input: 0,
   substitute_food_input: 0,
 };
-
-Papa.parse("file:///Users/dado/Projects/absolute_coaching_food_substitution_calculator/data.csv", {
-	download: true,
-	complete: function(results) {
-		console.log(results);
-	}
-});
 
 function calculate() {
   var amountToBeSubstituted = document.getElementById('amount_to_be_substituted_input').value;
@@ -48,18 +42,100 @@ function calculate() {
   document.getElementById('substitute_food_weight_output').value = result;
 }
 
-document.getElementById('food_to_be_substituted_input_autocomplete_list')
+function dataToDataList(data, dataListID) {
+  var dataList = document.getElementById(dataListID);
+  while (dataList.firstChild) {
+    dataList.removeChild(dataList.firstChild);
+  }
+  for (var i in data) {
+    var o = document.createElement("option");
+    o.value = data[i]['Name'];
+    dataList.append(o);
+  }
+}
 
-document.getElementById('food_to_be_substituted_input').onkeydown = function () {
-  console.log("YAY");
+function sortByScore(data) {
+
+}
+
+function stage2(value) {
+  for (var i in data) {
+    if (data[i]['Name'] == value) {
+      CALORIES_PER_GRAM['food_to_be_substituted_input'] = data[i]['Calories per gram'];
+      var ppg = data[i]['Protein per gram'];
+      var cpg = data[i]['Carbs per gram'];
+      var fpg = data[i]['Fat per gram'];
+      var cat = data[i]['Category'];
+      var stage2data = [];
+      for (var j in data) {
+        if (data[j]['Name'] != value) {
+          var ppg2 = data[j]['Protein per gram'];
+          var cpg2 = data[j]['Carbs per gram'];
+          var fpg2 = data[j]['Fat per gram'];
+          var cat2 = data[j]['Category'];
+          var score = Math.abs(ppg - ppg2) + Math.abs(cpg - cpg2) + Math.abs(fpg - fpg2);
+          if (cat == cat2) {
+            score = score * 0.50;
+          }
+          stage2data.push({ppg: ppg2, cpg: cpg2, fpg: fpg2, score: score, Name: data[j]['Name']});
+        }
+      }
+      console.log(stage2data.sort(function(a, b) {
+        return a.score - b.score;
+      }));
+
+      dataToDataList(stage2data.sort(function(a, b) {
+        return a.score - b.score;
+      }), 'substitute_food_input_autocomplete_list');
+    }
+  }
+}
+
+function setFoodToBeSubstituted() {
+  //get values in DOM
+  var value = document.getElementById('food_to_be_substituted_input').value;
+  var list = document.getElementById('food_to_be_substituted_input_autocomplete_list');
+  //Did the user choose a list item or are they still typing?
+  for (var i = 0; i < list.children.length; i++) {
+    if(list.children[i].value === value){
+      stage2(value);
+    }
+  }
+}
+
+function setSubstituteFood(){
+  var value = document.getElementById('substitute_food_input').value;
+  var list = document.getElementById('substitute_food_input_autocomplete_list');
+  //Did the user choose a list item or are they still typing?
+  for (var i = 0; i < list.children.length; i++) {
+    if(list.children[i].value === value){
+      for (var i in data) {
+        if (data[i]['Name'] == value) {
+          CALORIES_PER_GRAM['substitute_food_input'] = data[i]['Calories per gram'];
+        }
+      }
+    }
+  }
+}
+
+
+Papa.parse("https://raw.githubusercontent.com/DragosRotaru/Absolute-Coaching-Food-Substitution-Calculator/master/data.csv", {
+	download: true,
+  header: true,
+  dynamicTyping: true,
+	complete: function(results) {
+		data = results.data;
+    dataToDataList(data, 'food_to_be_substituted_input_autocomplete_list');
+	}
+});
+
+
+document.getElementById('food_to_be_substituted_input').oninput = function () {
+  setFoodToBeSubstituted();
 };
 
-document.getElementById('substitute_food_input').onkeydown = function () {
-  console.log("YAY");
-};
-
-document.getElementById('food_to_be_substituted_input').onselect = function () {
-  console.log("NAY");
+document.getElementById('substitute_food_input').oninput = function () {
+  setSubstituteFood();
 };
 
 document.getElementById('calculator_submit_button').onclick = calculate;
